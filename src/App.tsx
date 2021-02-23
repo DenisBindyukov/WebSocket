6f5f0bf8317f0110d398fa6397e43589175d4397
DenisBindyukov <gitgroom@gmail.com>
2021-02-23 18:33:22 +0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {ChangeEvent, DetailedHTMLProps, HTMLAttributes, KeyboardEvent, useEffect, useRef, useState} from 'react';
 import './App.css';
 
+type MessageEventType = {
+    userId: number,
+    userName: string
+    message: string
+    photo: string
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const messagesBlockRef = useRef<HTMLDivElement | null>(null);
+    const [messages, setMessage] = useState<string>('');
+    const [ws, setWS] = useState<WebSocket | null>(null);
+    const [users, setUsers] = useState<MessageEventType []>([]);
+
+
+
+    const ohChangeHandler = (e: ChangeEvent<HTMLTextAreaElement>) => {
+        setMessage(e.currentTarget.value)
+    }
+
+    const onKeyPressHandler = (e: KeyboardEvent<HTMLTextAreaElement>) => {
+        if (e.charCode === 13) {
+            sendMessage()
+            e.preventDefault()
+        }
+    }
+
+    const sendMessage = () => {
+        if( messages === '') {
+            return
+        }
+        ws && ws.send(messages)
+        setMessage('')
+    }
+
+
+    useEffect(() => {
+        let localWS = new WebSocket("wss://social-network.samuraijs.com/handlers/ChatHandler.ashx")
+        localWS.onmessage = (messageEvent) => {
+            const newMessage = JSON.parse(messageEvent.data)
+            setUsers(state => [...state,...newMessage])
+            messagesBlockRef.current && messagesBlockRef.current.scrollTo(0, messagesBlockRef.current. scrollHeight)
+        }
+        setWS(localWS)
+
+    }, [])
+
+    return (
+        <div className="App">
+            <div className={'chat'}  ref={messagesBlockRef}>
+                {
+                    users.map((u, index) =>  {
+                        return (
+                            <div className={'wrapper'} key={index}>
+                                <div>
+                                    <img
+                                        src={u.photo ? u.photo : "https://www.pavilionweb.com/wp-content/uploads/2017/03/man-300x300.png"}
+                                        alt="Picture"/>
+                                </div>
+                                <div>
+                                    <h3>{u.userName}</h3>
+                                    <span> {u.message} </span>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+
+                <div className={'footer'}>
+                    <div className={'wrapper'}>
+                        <div>
+                            <textarea value={messages} onChange={ohChangeHandler} onKeyPress={onKeyPressHandler} autoFocus/>
+                        </div>
+                        <div>
+                            <button style={{margin: '15px'}} onClick={sendMessage}>Click</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+    );
 }
 
 export default App;
